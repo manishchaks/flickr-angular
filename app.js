9e784e8e6aee7b0172249813636b354227b59299
Manish Chakravarty <manishchaks@gmail.com>
2014-09-15 18:48:57 +0530

    Created sample JSON structure which matches JSON output by Flickr.

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 (function () {
     var app = angular.module('flickr-angular', [])
     app.controller('ImageController', function () {
-        this.picture = pic;
+        this.photos = photos;
     });
 
-    var pic = {
-        name: 'FoobarGem',
-        price: 2.95,
-        description: 'This is a test description',
-        canPurchase: true
+    var photos = {
+        page: 1,
+        pages: 4483,
+        perpage: 100,
+        photo:[
+            {
+                farm: 6,
+                id: "15246462712",
+                isfamily: 0,
+                isfriend: 0,
+                ispublic: 1,
+                owner: "31600622@N06",
+                secret: "fd242a9b99",
+                server: "5585",
+                title: 	"P1040172"
+            },
+            {
+                farm: 6,
+                id: "15246462712",
+                isfamily: 0,
+                isfriend: 0,
+                ispublic: 1,
+                owner: "31600622@N06",
+                secret: "fd242a9b99",
+                server: "5585",
+                title: 	"P1040174"
+            }
+        ]
     }
 })();
 
